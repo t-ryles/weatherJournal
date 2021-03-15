b10e3500a0b00ba4fccbb4354ba182cfcfba025d
t-ryles <tajhakeem.ryles@gmail.com>
2021-03-14 22:20:25 -0500

    API  functioning
    Got the API setup and is now receiving the proper data

--- server.js
+++ server.js
@@ -1,56 +1,50 @@
+//Project Endpoint
+const projectData = {};
+
 // Module dependencies.
 const express = require('express');
 const bodyParser= require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const fetch = require("node-fetch");
+const { response } = require('express');
+const { default: fetch } = require('node-fetch');
 
 const app = express();
 
-//Middleware
-app.use(express.json());
-app.use(cors());
 
-// Body parser middleware
-app.use(express.urlencoded({extended: false}));
-
-//Project Endpoint
-const projectData = {};
-
-//API Key
+//API variables
 var APIKey = '0f4c4e121f7fc9cce5b749ee1f57fb90';
+var zipCode = '68503';
 
 //URL
-const url =`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${APIKey}`;
+const url =`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${APIKey}`;
 
-//Server
-// const http = require('http');
-// const { response } = require('express');
 
+//Middleware
+app.use(cors());
+
+//Bring in the weatherApp folder
+//app.use(express.static(path.join(__dirname, 'weatherApp')));
+
+// Body parser middleware
+app.use(express.urlencoded({extended: true}));
+
+//Port setup
 const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 
+//Fetching the openweather API data
+weatherData().catch(error => {
+  console.log('error!');
+  console.error(error);
+})
+
+async function weatherData () {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.main.temp);
+  console.log(data.main.feels_like);
+  return data;
+};
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
-
-const newLocal = 'index.html';
-app.use(express.static(path.join(__dirname, newLocal)));
-
-// weatherInfo();
-// //var zipCode = '524901';
-
-// async function weatherInfo() {
-//   try {
-//       const yes = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${APIKey}`) 
-//           const data = await yes;
-//           response.send(data);
-//           console.log('Fetch is working');
-//       } catch(e) {
-//         console.log(e);
-//     }
-// };
-
-app.get('/index.html', (req, res) => res.render('index', {
-  
-}));
-
-module.exports = app;
