acde2e35d930b3da5c0a472df4b71209280569bb
t-ryles <tajhakeem.ryles@gmail.com>
2021-03-27 13:20:16 -0500

    Server side update
    Removed/replaced some sever side code.
    Committed out unused CCS code.

--- server.js
+++ server.js
@@ -5,48 +5,73 @@ const projectData = {};
 const express = require('express');
 const bodyParser= require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const { default: fetch } = require('node-fetch');
-const zipCode = require('./server');
-const { json } = require('body-parser');
+const zipCode = require('/weatherApp/JS/app.js');
 
-
-console.log('this should log the ' + zipCode);
+//Instance of app
 const app = express();
 
+// Body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-//API variables
-var APIKey = '568078774ee955d4329869fe95c21137';
-
-//URL
-const url =`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${APIKey}`;
-
-
-//Middleware
+//Cors middleware
 app.use(cors());
 
 //Bring in the weatherApp folder
-//app.use(express.static(path.join(__dirname, 'weatherApp')));
+app.use(express.static(path.join(__dirname, 'weatherApp')));
 
-// Body parser middleware
-app.use(express.urlencoded({extended: true}));
+//API variables
+const APIKey = '';
+//let zipCode = '68503';
+//URL
+const url =`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${APIKey}`;
 
 //Port setup
 const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 
 //Fetching the openweather API data
-weatherData().catch(error => {
-  console.log('error!');
-  console.error(error);
-})
+// weatherData().catch(error => {
+//   console.log('error!');
+//   console.error(error);
+// })
 
-async function weatherData () {
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
-  // console.log(data.main.temp);
-  // console.log(data.main.feels_like);
-  return data;
-};
+// async function weatherData () {
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   console.log(data);
+//   // console.log(data.main.temp);
+//   // console.log(data.main.feels_like);
+//   return data;
+// };
+
+const getWeather = async(url) => {
+  const res = await fetch(url) 
+  try {
+    const data = await res.json();
+    console.log(data);
+  }catch(error) {
+    console.log('error', error);
+  }
+}
+// console.log(getWeather);
+
+// POST route
+// app.post('./weatherApp/index.html', callBack);
+
+// function callBack(req,res){
+//   res.send('POST received');
+// };
+
+// //Post data
+// const data = [];
+
+// app.post('/weatherApp/index.html', addWeatherData);
+
+// function addWeatherData (req,res){
+//     data.push(req.body);
+// };
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
--- weatherApp/JS/app.js
+++ weatherApp/JS/app.js
@@ -1,18 +1,17 @@
 //Setting global var
 const feelingsInput = document.getElementById('feelings');
 const generateBtn = document.getElementById('generate');
-const body = document.querySelector('body');
 
 
 //Add click event to button
-generateBtn.addEventListener('click', inputZipCode
-);
+generateBtn.addEventListener('click', inputZipCode);
 //User zip code inupt
 function inputZipCode() {
     var zipCode = document.getElementById('zip').value;
     console.log(zipCode);
-    return zipCode
+    return zipCode;
 };
 
-
-module.exports = zipCode.value;
+module.exports = { 
+    zipCode
+};
